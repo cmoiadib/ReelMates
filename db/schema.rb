8e1f2d40f885a72b2d4995ec907d5f06108ede75
Paul Cabot <paul.claude.cabot@gmail.com>
2024-11-25 17:39:39 +0100

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_112128) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "saved_settings", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
