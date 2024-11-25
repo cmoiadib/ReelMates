27a48bda729e7ced066b75ea671250417840ca4a
Paul Cabot <paul.claude.cabot@gmail.com>
2024-11-25 17:40:52 +0100

    Guest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_112128) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_25_164022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guests", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
