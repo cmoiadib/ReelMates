689aa26e8cd06b724b65bf718eb9fc6070cc1507
parwez amini <cmoiadib@gmail.com>
2024-11-27 15:25:48 +0100

    basic version of the API integration with conditions

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -1,4 +1,26 @@
+require "open-uri"
+require "json"
+
 class SwipesController < ApplicationController
+  def index
+    @party = Party.find(params[:party_id])
+    @providers = @party.platform_setting
+    @categories = @party.category_setting
+    @start_year = @party.start_year
+    @end_year = @party.end_year
+
+    @url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&watch_region=FR"
+
+    @url += "&with_watch_providers=#{@providers.join('|')}" if @providers.present?
+    @url += "&with_genres=#{@categories.join('|')}" if @categories.present?
+    @url += "&primary_release_date.gte=#{@start_year}-01-01" if @start_year.present?
+    @url += "&primary_release_date.lte=#{@end_year}-12-31" if @end_year.present?
+    @url += "&api_key=#{ENV['TMDB_API_KEY']}"
+
+    response = JSON.parse(URI.open(@url).read)
+    @movies = response["results"]
+  end
+
   def show
     @swipe = Swipe.find(params[:id])
   end
--- app/models/party.rb
+++ app/models/party.rb
@@ -4,5 +4,40 @@ class Party < ApplicationRecord
   has_many :users, through: :party_players
   has_many :swipes, through: :party_players
 
-  PROVIDERS = [ "Netflix", "Prime", "HBO", "OCS", "Apple TV+", "Disney+"]
+  PROVIDERS = [
+    { name: "Netflix", id: "8" },
+    { name: "Prime", id: "119" },
+    { name: "Apple TV+", id: "350" },
+    { name: "Disney+", id: "337" }
+  ]
+
+  GENRES = [
+    { name: "Action", id: 28 },
+    { name: "Adventure", id: 12 },
+    { name: "Animation", id: 16 },
+    { name: "Comedy", id: 35 },
+    { name: "Crime", id: 80 },
+    { name: "Documentary", id: 99 },
+    { name: "Drama", id: 18 },
+    { name: "Family", id: 10751 },
+    { name: "Fantasy", id: 14 },
+    { name: "History", id: 36 },
+    { name: "Horror", id: 27 },
+    { name: "Music", id: 10402 },
+    { name: "Mystery", id: 9648 },
+    { name: "Romance", id: 10749 },
+    { name: "Science Fiction", id: 878 },
+    { name: "TV Movie", id: 10770 },
+    { name: "Thriller", id: 53 },
+    { name: "War", id: 10752 },
+    { name: "Western", id: 37 }
+  ]
+
+  def platform_setting=(values)
+    super(Array(values))
+  end
+
+  def category_setting=(values)
+    super(Array(values))
+  end
 end
