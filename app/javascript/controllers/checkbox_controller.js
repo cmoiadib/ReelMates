264991d08a2e136ccd0be5bb486e22ed43ada305
parwez amini <cmoiadib@gmail.com>
2024-12-02 09:09:56 +0100

    made some changes

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -3,6 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["all", "checkbox"]
 
+  connect() {
+    const checkboxes = this.element.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+        checkbox.parentElement.classList.toggle('selected', checkbox.checked);
+      });
+    });
+  }
+
   toggleAll() {
     const checked = this.allTarget.checked
     this.checkboxTargets.forEach(checkbox => {
