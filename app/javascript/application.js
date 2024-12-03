ea6fac64ce111a6ae9c1e8092df51b0c572b0672
Emilie Didier <emilie.didier97@gmail.com>
2024-12-03 10:26:08 +0100

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./controllers/loading_controller"
-import "./controllers/application"
-import "./controllers/results_controller"
 import "@popperjs/core"
 import "bootstrap"
 import "@rails/actioncable"
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,9 +2,3 @@
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-import LoadingController from "./loading_controller"
-import ResultsController from "./results_controller"
-
-application.register("loading", LoadingController)
-application.register("results", ResultsController)
