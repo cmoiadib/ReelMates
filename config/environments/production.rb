a4ed5bb8e1d1d604819dfed59a8123518f5a7aa4
diane johnston <diane.jroussillon@gmail.com>
2024-12-03 12:51:03 +0100

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,11 +28,7 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.digest = true
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "https://reel-mates-test-3df5e8ab2fe0.herokuapp.com/"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
